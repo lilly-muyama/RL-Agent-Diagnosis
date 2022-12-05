2bc7cef915edfb54bbc745527d624d0d0b93f04e
Lillian Muyama <lmuyama@PL823-PRO.paris.inria.fr>
2022-12-05 19:31:35 +0100

    building lupus env

--- lupus/modules/constants.py
+++ lupus/modules/constants.py
@@ -1,1 +1,28 @@
+import numpy as np
+
 SEED = 42
+FEATURE_NUM = 26
+GYM_BOX_LOW = -1
+GYM_BOX_HIGH = np.inf
+ACTION_SPACE = ['Negative', 'Positive', 'Inconclusive diagnosis', 'ana', 'fever', 'leukopenia', 'thrombocytopenia', 'auto_immune_hemolysis', 'delirium', 
+'psychosis', 'seizure', 'non_scarring_alopecia', 'oral_ulcers', 'subacute_cutaneous', 'discoid_lupus', 'acute_cutaneous_lupus', 'pleural_effusion', 
+'pericardial_effusion', 'acute_pericarditis', 'joint_involvement', 'proteinuria', 'renal_biopsy_class', 'anti_cardioliphin_antibodies', 
+'anti_b2gp4_antibodies', 'lupus_anti_coagulant', 'c3', 'c4', 'anti_dsdna_antibody', 'anti_smith_antibody' ]
+MAX_STEPS = 10
+CLASS_NUM = 3
+ACTION_NUM = FEATURE_NUM + CLASS_NUM
+CLASS_DICT = {'Negative':0, 'Positive':1, 'Inconclusive diagnosis':2} 
+DOMAINS_FEAT_DICT = {'constitutional': ['fever'], 'hematologic': ['leukopenia', 'thrombocytopenia', 'auto_immune_hemolysis'], 
+'neuropsychiatric': ['delirium', 'psychosis', 'seizure'], 
+'mucocutaneous': ['non_scarring_alopecia', 'oral_ulcers', 'subacute_cutaneous', 'discoid_lupus', 'acute_cutaneous_lupus'],
+'serosal': ['pleural_effusion', 'pericardial_effusion', 'acute_pericarditis'], 'musculoskeletal': ['joint_involvement'], 
+'renal': ['proteinuria', 'renal_biopsy_class'], 
+'antiphospholipid_antibodies': ['anti_cardioliphin_antibodies', 'anti_b2gp4_antibodies', 'lupus_anti_coagulant'],
+'complement_proteins': ['c3', 'c4'], 'sle_specific_antibodies':['anti_dsdna_antibody', 'anti_smith_antibody']}
+DOMAINS_MAX_SCORES_DICT = {'constitutional': 2, 'hematologic': 4, 'neuropsychiatric': 5, 'mucocutaneous': 6, 'serosal': 6, 'musculoskeletal': 6, 'renal': 10, 
+'antiphospholipid_antibodies': 2, 'complement_proteins': 4, 'sle_specific_antibodies':6}
+CRITERIA_WEIGHTS = {'fever':2, 'leukopenia':3, 'thrombocytopenia':4, 'auto_immune_hemolysis':4, 'delirium':2, 'psychosis':3, 'seizure':5, 
+'non_scarring_alopecia':2, 'oral_ulcers':2, 'subacute_cutaneous':4, 'discoid_lupus':4, 'acute_cutaneous_lupus':6, 'pleural_effusion': 5, 
+'pericardial_effusion':5, 'acute_pericarditis':6, 'joint_involvement':6, 'proteinuria':4, 'renal_biopsy_2_5':8, 'renal_biopsy_3_4': 10, 
+'anti_cardioliphin_antibodies':2, 'anti_b2gp4_antibodies':2, 'lupus_anti_coagulant':2, 'low_c3_or_low_c4':3, 'low_c3_and_low_c4':4, 
+'anti_dsdna_antibody':6, 'anti_smith_antibody':6,}
---
+++ lupus/modules/utils.py
@@ -0,0 +1,70 @@
+import pandas as pandas
+import numpy as np
+
+
+domains_feat_dict = constants.DOMAINS_FEAT_DICT
+domains_max_scores_dict = constants.DOMAINS_MAX_SCORES_DICT
+criteria_weights = constants.CRITERIA_WEIGHTS
+
+def get_domain_score(row, domain):
+    domain_features = domains_feat_dict[domain]
+    domain_score = 0
+    if domain == 'complement_proteins':
+        domain_score = get_c3_c4_score(row.c3, row.c4)
+        domain_features = list(set(domain_features) - set(['c3', 'c4']))
+    for feat in domain_features:
+        if feat == 'renal_biopsy_class': # to delete
+            pass
+        else:
+            if row[feat] >= 0:
+                feat_score = get_feat_score(row, feat)
+                if feat_score > domain_score:
+                    domain_score = feat_score
+    if domain_score > domains_max_scores_dict[domain]:
+        raise Exception('The score is too large for this domain!')
+    return domain_score
+
+def get_c3_c4_score(c3, c4): # 0 - low, 1 is not low, -1 is unknown
+    if (c3 == 0) & (c4 == 0):
+        return criteria_weights['low_c3_and_low_c4']
+    elif (c3 == 0) | (c4 == 0):
+        return criteria_weights['low_c3_or_low_c4']
+    else:
+        return 0
+
+def get_feat_score(row, feat):
+    if feat == 'proteinuria':
+        feat_score = get_proteinura_score(row[feat])
+    elif feat == 'renal_biopsy_class':
+        feat_score = get_renal_biopsy_score(row[feat])
+    elif row[feat] <= 0:
+        feat_score = 0
+    else:
+        feat_score = criteria_weights[feat]
+    return feat_score
+
+def get_proteinura_score(amount):
+    if amount > 0.5:
+        return 4
+    else:
+        return 0
+
+def get_renal_biopsy_score(result_class):
+    if (result_class == 3) | (result_class == 4):
+        return 10
+    elif (result_class == 2) | (result_class == 5):
+        return 8
+    else:
+        return 0
+
+def compute_score(state):
+    df = pd.DataFrame(columns = constants.ACTION_SPACE[constants.CLASS_NUM:])
+    df = df.append(state)
+    row = df.iloc[0]
+    if row['ana'] == 0: # negative - 0 positive - 1
+        return 0
+    total_row_score = 0
+    for domain in domains_feat_dict.keys():
+        domain_score = get_domain_score(row, domain)
+        total_row_score += domain_score
+    return total_row_score
