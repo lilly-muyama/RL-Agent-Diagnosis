23f83bd8320b65dcef0f0eb9377c6098fe8bc7ba
Lillian Muyama <lmuyama@PL1207-PRO.paris.inria.fr>
2023-02-10 17:17:02 +0100

    more models

--- lupus/modules/simple_constants.py
+++ lupus/modules/simple_constants.py
@@ -24,14 +24,14 @@ SEED = 42
 # 'non_scarring_alopecia', 'leukopenia', 'delirium']
 
 #11 FEATURES DATASET
-# ACTION_SPACE = ['No lupus', 'Lupus', 'Inconclusive diagnosis', 'ana', 'anti_dsdna_antibody', 'joint_involvement', 'proteinuria', 'pericardial_effusion',
-# 'non_scarring_alopecia', 'leukopenia', 'delirium', 'low_c3_and_c4', 'fever', 'anti_cardioliphin_antibodies']
+ACTION_SPACE = ['No lupus', 'Lupus', 'Inconclusive diagnosis', 'ana', 'anti_dsdna_antibody', 'joint_involvement', 'proteinuria', 'pericardial_effusion',
+'non_scarring_alopecia', 'leukopenia', 'delirium', 'low_c3_and_c4', 'fever', 'anti_cardioliphin_antibodies']
 
 #22 FEATURES DATASET
-ACTION_SPACE =  ['No lupus', 'Lupus', 'Inconclusive diagnosis', 'ana', 'non_scarring_alopecia', 'anti_dsdna_antibody', 'joint_involvement', 
-'proteinuria', 'pericardial_effusion', 'leukopenia', 'delirium', 'low_c3', 'low_c4', 'fever', 'thrombocytopenia', 'anti_cardioliphin_antibodies', 
-'pleural_effusion', 'psychosis', 'seizure', 'lupus_anti_coagulant', 'anti_β2gp1_antibodies', 'anti_smith_antibody',  'oral_ulcers', 
-'auto_immune_hemolysis', 'acute_pericarditis'] #cutaneous lupus left out
+# ACTION_SPACE =  ['No lupus', 'Lupus', 'Inconclusive diagnosis', 'ana', 'non_scarring_alopecia', 'anti_dsdna_antibody', 'joint_involvement', 
+# 'proteinuria', 'pericardial_effusion', 'leukopenia', 'delirium', 'low_c3', 'low_c4', 'fever', 'thrombocytopenia', 'anti_cardioliphin_antibodies', 
+# 'pleural_effusion', 'psychosis', 'seizure', 'lupus_anti_coagulant', 'anti_β2gp1_antibodies', 'anti_smith_antibody',  'oral_ulcers', 
+# 'auto_immune_hemolysis', 'acute_pericarditis'] #cutaneous lupus left out
 
 
 CLASS_DICT = {'No lupus':0, 'Lupus':1, 'Inconclusive diagnosis':2}
